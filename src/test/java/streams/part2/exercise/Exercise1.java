d7a44594db8c3069b23ad4d037539f7d02f10785
Anna Iushina <anna.iushina@Kronos.com>
2018-10-18 15:39:52 +0300

    Stream part 2, exercise 1 implementation

--- src/test/java/streams/part2/exercise/Exercise1.java
+++ src/test/java/streams/part2/exercise/Exercise1.java
@@ -5,11 +5,9 @@ import lambda.data.JobHistoryEntry;
 import lambda.data.Person;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
@@ -24,8 +22,16 @@ class Exercise1 {
     void calcTotalYearsSpentInEpam() {
         List<Employee> employees = getEmployees();
 
-        // TODO реализация
-        Long hours = null;
+        Long hours =
+        employees.stream()
+                .map(Employee::getJobHistory)
+                .mapToLong(jobHistoryEntries -> jobHistoryEntries.stream()
+                                            .filter(jobHistoryEntry -> jobHistoryEntry.getEmployer().equals("EPAM"))
+                                            .mapToInt(JobHistoryEntry::getDuration)
+                                            .mapToLong(Long::new)
+                                            .sum())
+                .sum();
+
 
         assertThat(hours, is(19L));
     }
@@ -34,8 +40,14 @@ class Exercise1 {
     void findPersonsWithQaExperience() {
         List<Employee> employees = getEmployees();
 
-        // TODO реализация
-        Set<Person> workedAsQa = null;
+        Set<Person> workedAsQa =
+        employees.stream()
+                .filter(employee -> employee.getJobHistory()
+                                            .stream()
+                                            .anyMatch(jobHistoryEntry -> jobHistoryEntry.getPosition().equals("QA"))
+                )
+                .map(Employee::getPerson)
+                .collect(Collectors.toSet());
 
         assertThat(workedAsQa, containsInAnyOrder(
                 employees.get(2).getPerson(),
@@ -48,8 +60,12 @@ class Exercise1 {
     void composeFullNamesOfEmployeesUsingLineSeparatorAsDelimiter() {
         List<Employee> employees = getEmployees();
 
-        // TODO реализация
-        String result = null;
+        String result =
+        employees.stream()
+                .map(Employee::getPerson)
+                .map(Person::getFullName)
+                .collect(Collectors.joining("\n"));
+
 
         assertThat(result, is(
                 "Иван Мельников\n"
@@ -65,8 +81,20 @@ class Exercise1 {
     void groupPersonsByFirstPositionUsingToMap() {
         List<Employee> employees = getEmployees();
 
-        // TODO реализация
-        Map<String, Set<Person>> result = null;
+        Map<String, Set<Person>> result =
+        employees.stream()
+                .collect(Collectors.toMap(
+                        employee -> employee.getJobHistory().get(0).getPosition()
+                        ,
+                        (Employee employee) -> {
+                        Set<Person> setOfOnePerson = new HashSet<>();
+                                setOfOnePerson.add(employee.getPerson());
+                                return setOfOnePerson;}
+                        ,
+                        (set1, set2) -> {
+                                set1.addAll(set2);
+                                return set1;
+                        }));
 
         assertThat(result, hasEntry(is("dev"), contains(employees.get(0).getPerson())));
         assertThat(result, hasEntry(is("QA"), containsInAnyOrder(employees.get(2).getPerson(), employees.get(5).getPerson())));
@@ -78,8 +106,10 @@ class Exercise1 {
     void groupPersonsByFirstPositionUsingGroupingByCollector() {
         List<Employee> employees = getEmployees();
 
-        // TODO реализация
-        Map<String, Set<Person>> result = null;
+        Map<String, Set<Person>> result =
+        employees.stream()
+                .collect(Collectors.groupingBy(employee -> employee.getJobHistory().get(0).getPosition(),
+                        Collectors.mapping(Employee::getPerson, Collectors.toSet())));
 
         assertThat(result, hasEntry(is("dev"), contains(employees.get(0).getPerson())));
         assertThat(result, hasEntry(is("QA"), containsInAnyOrder(employees.get(2).getPerson(), employees.get(5).getPerson())));
